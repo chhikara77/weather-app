ab78f3af1cc5e2e1dc98bffd696a723b381932af
Sanjay Chhikara <sanjaychhikara77@gmail.com>
2024-01-29 14:12:19 +0530

    added some changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [city,setCity] = useState(null);
+
+  useEffect(() => {
+    
+  }, []);
+
+  const getData = () =>{
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3ba690e3234fdbdba1669f481c481e6c`); 
+        const result = await response.json();
+        console.log(result)
+        //setData(result);
+      } catch (error) {
+        //setError(error);
+        console.log('erroe',error)
+      } finally {
+       // setLoading(false);
+       console.log('erroe1')
+      }
+    };
+
+    fetchData();
+  }
+
+  // if (loading) {
+  //   return <p>Loading...</p>;
+  // }
+
+  // if (error) {
+  //   return <p>Error: {error.message}</p>;
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Weather in your city</h1>
+       <input onChange={(e)=>setCity(e.target.value)} type="text" />
+       <button onClick={getData}><span className='question-icon'>?</span>Search</button>
       </header>
     </div>
   );
---
+++ src/components/WeatherInfo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const WeatherInfo = () => {
+  return (
+    <div>
+        <div>Date</div>
+        <div>temp</div>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default WeatherInfo
